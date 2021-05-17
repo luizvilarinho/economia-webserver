a3b6f080213eb56807b93507ac739b44e45f8640
luizvilarinho <luizvilarinho@gmail.com>
2021-05-17 19:01:15 -0300

    fix:cors try

--- src/index.js
+++ src/index.js
@@ -25,13 +25,13 @@ app.use(express.json());
 app.use("/api/eco/v1/data", basics);
 app.use("/api/eco/v1/login", login);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "*");
-  res.header('Access-Control-Allow-Credentials', "*");
-  res.header('Access-Control-Expose-Headers', 'x-access-token'); //essta linha habilita o token no header
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   res.header('Access-Control-Allow-Credentials', "*");
+//   res.header('Access-Control-Expose-Headers', 'x-access-token'); //essta linha habilita o token no header
+//   next();
+// });
 
 const port = process.env.PORT || 2500;
 
